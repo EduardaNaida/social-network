8dce192883a1f761be492d22eff1169ba2a2bfe9
EduardaNaida <eduarda.naida@gmail.com>
2022-09-30 16:56:55 +0300

    added path to dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
+
 type DialogsType = {}
 export const Dialogs = (props: DialogsType) => {
     return (
-        <div>
-            <div>
-                Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogItem}>
+                <div className={s.dialog}>
+                    <NavLink to={'/dialogs/0'}  activeClassName={s.active}>Eduarda</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to={'/dialogs/1'}  activeClassName={s.active}>Artiom</NavLink>
+                </div>
+               <div className={s.dialog}>
+                   <NavLink to={'/dialogs/2'}  activeClassName={s.active}>Dasha</NavLink>
+               </div>
+                <div className={s.dialog}>
+                    <NavLink to={'/dialogs/3'}  activeClassName={s.active}>Ivan</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to={'/dialogs/4'}  activeClassName={s.active}>Ekaterina</NavLink>
+                </div>
+
+            </div>
+            <div className={s.messages}>
+                <div className="message">Hi!</div>
+                <div className="message">How are you?</div>
+                <div className="message">I'm fine!</div>
             </div>
         </div>
     );
